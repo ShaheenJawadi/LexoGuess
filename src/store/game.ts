2e409e759f324016d07c6ef986e7414aa44822c1
shaheenj <jawadishaheen@gmail.com>
2024-01-24 03:00:18 +0100

    Create Async Thunk for Data Fetching

--- src/store/game.ts
+++ src/store/game.ts
@@ -1,6 +1,17 @@
 import { entredWordsType, keyboardDataType, letterStatus } from '@/types'
-import { createSlice } from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import axios from "axios";
 
+
+export const handleSubmit = createAsyncThunk(
+  "appGame/handleSubmit",
+  async () => {
+      
+    return true;
+
+  }
+);
+ 
 
 type initialStateType= {
   word:string , 
@@ -20,109 +31,89 @@ const initialState:initialStateType={
   line :0 ,
 
 }
-
-
+ 
 export const appGameSlice = createSlice({
-  name: 'appGame',
+  name: "appGame",
   initialState,
   reducers: {
-   
     handleAddLetter: (state, action) => {
       let entredWords = state.entredWords;
-      if(state.line<6){
-        if(entredWords[state.line]===undefined){
-          entredWords.push([{
-            alpha:action.payload , status:''
-          }])
-
-        }
-        else if (entredWords[state.line].length<5) {
-          
+      if (state.line < 6) {
+        if (entredWords[state.line] === undefined) {
+          entredWords.push([
+            {
+              alpha: action.payload,
+              status: "",
+            },
+          ]);
+        } else if (entredWords[state.line].length < 5) {
           entredWords[state.line].push({
-            alpha:action.payload , status:''
-          })
+            alpha: action.payload,
+            status: "",
+          });
         }
       }
 
-
-     
-        state.entredWords =entredWords
-
-      
+      state.entredWords = entredWords;
     },
     handleBackspace: (state) => {
       let entredWords = state.entredWords;
-      if(entredWords[state.line] !==undefined){
-        entredWords[state.line].pop()
-
-
-      } 
-        state.entredWords =entredWords
-
-
-      
-    },
-    handleSubmit: (state) => {
-      let entredWords = state.entredWords;
-      let keyboardData = state.keyboardData;
-      let isCorrect:boolean = true  ;
-      
-      if(entredWords[state.line] !==undefined &&entredWords[state.line].length==5){
-        
-        
-        entredWords[state.line].map((value , index )=>{
-          let alpha=entredWords[state.line][index].alpha , status:"absent" | "present" | "correct"="absent";
-          if(state.word[index]== alpha){
-            status="correct";
-            keyboardData.correct+=alpha;
-
-
-          }
-          else {
-            isCorrect=false ; 
-
-            if(state.word.indexOf(alpha)!=-1){
-              status="present";
-              keyboardData.present+=alpha;
-
-            }
-            else {
-              status="absent";
-              keyboardData.absent+=alpha;
-            }
-           
-          }
-          entredWords[state.line][index].status=status
-          keyboardData[status]+=alpha
-
-        })
-
-        state.entredWords =entredWords
-       
-
-
-        
-        if(isCorrect){
-          alert('correct')
-        }
-        else if(state.line<5){
-          state.line++
-        }
-        else {
-          alert('GameOver')
-        }
-
+      if (entredWords[state.line] !== undefined) {
+        entredWords[state.line].pop();
       }
-
-      else {
-        alert('No')
-      }
-      
+      state.entredWords = entredWords;
     },
+    // handleSubmit: (state) => {
+    //   let entredWords = state.entredWords;
+    //   let keyboardData = state.keyboardData;
+    //   let isCorrect: boolean = true;
 
-    
-  }
-})
+    //   if (
+    //     entredWords[state.line] !== undefined &&
+    //     entredWords[state.line].length == 5
+    //   ) {
+    //     entredWords[state.line].map((value, index) => {
+    //       let alpha = entredWords[state.line][index].alpha,
+    //         status: "absent" | "present" | "correct" = "absent";
+    //       if (state.word[index] == alpha) {
+    //         status = "correct";
+    //         keyboardData.correct += alpha;
+    //       } else {
+    //         isCorrect = false;
+
+    //         if (state.word.indexOf(alpha) != -1) {
+    //           status = "present";
+    //           keyboardData.present += alpha;
+    //         } else {
+    //           status = "absent";
+    //           keyboardData.absent += alpha;
+    //         }
+    //       }
+    //       entredWords[state.line][index].status = status;
+    //       keyboardData[status] += alpha;
+    //     });
+
+    //     state.entredWords = entredWords;
+
+    //     if (isCorrect) {
+    //       alert("correct");
+    //     } else if (state.line < 5) {
+    //       state.line++;
+    //     } else {
+    //       alert("GameOver");
+    //     }
+    //   } else {
+    //     alert("No");
+    //   }
+    // },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(handleSubmit.fulfilled, (state, action) => {
+            
+      console.log(action)
+    });
+  },
+});
  
-export const { handleAddLetter ,handleBackspace ,handleSubmit} = appGameSlice.actions
+export const { handleAddLetter ,handleBackspace} = appGameSlice.actions
 export default appGameSlice.reducer
