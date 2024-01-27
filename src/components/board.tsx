a8cc4da91a3eb1ce363f1fbf7b0836e30b975aef
shaheenj <jawadishaheen@gmail.com>
2024-01-27 07:16:38 +0100

    fetch a random word

--- src/components/board.tsx
+++ src/components/board.tsx
@@ -1,12 +1,17 @@
 import { entredWordsType } from "@/types";
 import Cell from "./cell"
-import { useSelector } from "react-redux";
-import {  RootState } from "@/store"; 
-
- 
+import { fetchRandomWord } from "@/store/game";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "@/store";
 
 function Board( ) {
   const data = useSelector((state: RootState) => state.game)
+  const dispatch = useDispatch<AppDispatch>()
+  useEffect(()=>{
+     
+    dispatch(fetchRandomWord())
+  }, [dispatch])
 
     return (
       <>
