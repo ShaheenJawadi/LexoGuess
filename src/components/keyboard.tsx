e1e7a39a225276276a26f4595c9ca6ad766f845f
shaheenj <jawadishaheen@gmail.com>
2024-01-30 07:46:55 +0100

    Implement keyboard input handling

--- src/components/keyboard.tsx
+++ src/components/keyboard.tsx
@@ -5,7 +5,8 @@ import { checkLetter } from "@/utils";
 import { keyboardDataType } from "@/types";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "@/store";
-import { handleAddLetter,handleBackspace} from "@/store/game";
+import { handleSubmit, handleAddLetter,handleBackspace} from "@/store/game";
+import { useEffect } from "react";
 
 
 const  Keyboard=()=> {
@@ -14,6 +15,28 @@ const  Keyboard=()=> {
 
   const keys =  [ 'azertyuiop', 'qsdfghjklm', 'wxcvbn#' ]
   const store = useSelector((state: RootState) => state.game)
+  
+
+  useEffect(() => {
+    const handleKeyDown = (event: any) => {
+      const pressedKey:number = event.keyCode;
+
+
+       if ((pressedKey >= 65)&&(pressedKey <= 90)) {
+        dispatch(handleAddLetter((event.key as string).toLowerCase()));
+      } else if (pressedKey == 13) {
+         dispatch(handleSubmit())
+      }else if (pressedKey == 8) {
+        dispatch(handleBackspace());
+      } 
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []); 
   
   
     const clickKey=(letter:string)=>{
@@ -38,10 +61,10 @@ const  Keyboard=()=> {
             <div key={index} className="keyboardRow">
               {row
                 .split('')
-                .map((letter) =>
+                .map((letter , index) =>
                 letter=='#'?
                   (
-                      <div  onClick={()=>clickKey("#")} className={` keyboardButton spec`} >
+                      <div key={index} onClick={()=>clickKey("#")} className={` keyboardButton spec`} >
                         <Icon path={mdiBackspace}
                           size={0.8}
                         />
@@ -51,7 +74,7 @@ const  Keyboard=()=> {
                   :(
                     <Key
                     
-                      key={letter}
+                        key={index}
                       letterStatus={checkLetter(letter ,store.keyboardData)}
                       letter={letter} 
                       onClick={(l)=>clickKey(l)}
